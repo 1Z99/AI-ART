1778801fc88b70fc62dab0a450b3f1cf0ed950d1
Nawzat Sattout <85915502+1Z99@users.noreply.github.com>
2023-12-15 01:30:58 +0100

    Update scripts.js
    better js

--- scripts.js
+++ scripts.js
@@ -1,40 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-   let currentSlide = 0;
-   const slides = document.querySelectorAll('.slide');
-   const slideCount = slides.length;
- 
-   function hideAllSlides() {
-       slides.forEach(slide => {
-           slide.style.display = 'none';
-       });
-   }
- 
-   function showSlide(slideIndex) {
-       hideAllSlides();
-       slides[slideIndex].style.display = 'block';
-   }
- 
-   function prevSlide() {
-       currentSlide = (currentSlide - 1 + slideCount) % slideCount;
-       showSlide(currentSlide);
-   }
- 
-   function nextSlide() {
-       currentSlide = (currentSlide + 1) % slideCount;
-       showSlide(currentSlide);
-   }
- 
-   // Initial slide display
-   showSlide(currentSlide);
- 
-   // Event listeners for navigation buttons
-   document.getElementById('prevSlide').addEventListener('click', prevSlide);
-   document.getElementById('nextSlide').addEventListener('click', nextSlide);
- 
-   const phrases = ["Artrc", "Royal crown", "Nawzat"]; // Phrases to display
-   let index = 0; // Index to track phrases
- 
-   // Start the animation
- 
- });
- 
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.slide');
+    const slideCount = slides.length;
+    let autoSlideInterval; 
+
+    function hideAllSlides() {
+        slides.forEach(slide => {
+            slide.style.display = 'none';
+        });
+    }
+
+    function showSlide(slideIndex) {
+        hideAllSlides();
+        slides[slideIndex].style.display = 'block';
+    }
+
+    function prevSlide() {
+        currentSlide = (currentSlide - 1 + slideCount) % slideCount;
+        showSlide(currentSlide);
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slideCount;
+        showSlide(currentSlide);
+    }
+
+    showSlide(currentSlide);
+
+    
+    document.getElementById('prevSlide').addEventListener('click', prevSlide);
+    document.getElementById('nextSlide').addEventListener('click', nextSlide);
+
+    
+    function startAutoSlide() {
+        autoSlideInterval = setInterval(nextSlide, 1000);
+    }
+
+    startAutoSlide(); 
+
+    
+    slides.forEach(slide => {
+        slide.addEventListener('mouseenter', () => {
+            clearInterval(autoSlideInterval);
+        });
+        slide.addEventListener('mouseleave', () => {
+            startAutoSlide(); 
+        });
+    });
+});
